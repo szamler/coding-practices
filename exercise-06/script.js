0e7bafaa95a53d51aeee54399dcab4bfc16bef3f
szamler <63117451+szamler@users.noreply.github.com>
2021-02-25 23:24:56 -0500

    sz

---
+++ exercise-06/script.js
@@ -0,0 +1,6 @@
+let noun = ['dog','cat','girl','cup','plate'];
+let verb = ['run', 'walk', 'jump', 'skip', 'swim'];
+let adjective = ['quickly', 'shakily', 'weirdly', 'slowly', 'quizically'];
+
+let sentence = `My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky`;
+console.log(sentence)
