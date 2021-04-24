9b0ee1af033d0bdc3a1709dec5cafa2564ad1a06
szamler <63117451+szamler@users.noreply.github.com>
2021-04-23 23:43:43 -0400

    edits

--- project-04/script.js
+++ project-04/script.js
@@ -1,10 +1,20 @@
-$(document).ready(function() {
-  $("#splash").append("<img src='assets/banner.gif"+ Math.random() + "'/>");
-})
-// external js: masonry.pkgd.js
-
-$('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: 160
+window.onload = function() {
+  document.querySelectorAll(".list .image").forEach((drink) => {
+    drink.addEventListener('click', drinkSelect);
+  });
+  document.querySelectorAll(".mug .imgwrapper1, .mug .imgwrapper2,.mug .imgwrapper3, .mug .imgwrapper4, .mug .imgwrapper5").forEach((mug) => {
+    mug.addEventListener('click', mugSelect);
   });
   
+}
+
+function drinkSelect(element) {
+document.querySelector("#selectedDrink").src = element.srcElement.src
+  
+}
+
+function mugSelect(element) {
+  document.querySelector("#selectedMug").src = element.srcElement.src;
+  document.querySelector(".result").scrollIntoView()
+}
+
