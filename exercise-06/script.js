04e8888056018ab5b77c2d61002a76c2c363b923
szamler <63117451+szamler@users.noreply.github.com>
2021-04-25 12:31:14 -0400

    edits

--- exercise-06/script.js
+++ exercise-06/script.js
@@ -1,6 +1,41 @@
-let noun = ['dog','cat','girl','cup','plate'];
-let verb = ['run', 'walk', 'jump', 'skip', 'swim'];
-let adjective = ['quickly', 'shakily', 'weirdly', 'slowly', 'quizically'];
-
-let sentence = `My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky`;
-console.log(sentence)
+let array_one = [
+    "a house of wood",
+    "a house of brick",
+    "a house of broken dishes",
+    "a house of discarded clothing",
+    "a house of dust"
+  ];
+  
+  let array_two = [
+    "among other houses",
+    "among small hills",
+    "among high montains",
+    "by a river",
+    "by an abandoned lake"
+  ];
+  
+  // let randomElement = arrayName[Math.floor(Math.random()*arrayName.length)];
+  
+  function renderPoem() {
+    let element_from_array_one =
+      array_one[Math.floor(Math.random() * array_one.length)];
+  
+    let element_from_array_two =
+      array_two[Math.floor(Math.random() * array_two.length)];
+  
+    // grab div from html
+    let poem = document.querySelector(".poem");
+  
+    // create a new element (paragraph)
+    let paragraph = document.createElement("p");
+    paragraph.textContent = element_from_array_one + " " + element_from_array_two;
+  
+    // append the p to the div
+    poem.appendChild(paragraph);
+  
+    // console.log(element_from_array_one + " " + element_from_array_one)
+    console.log(`${element_from_array_one} \n ${element_from_array_two}`);
+  }
+  
+  let btn = document.querySelector("button");
+  btn.addEventListener("click", renderPoem);
